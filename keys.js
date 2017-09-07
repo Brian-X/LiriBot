68574ae255d20a9d7f87e33efc9a64e04e0a8449
Brian-X <brian@bae-media.com>
2017-09-06 21:40:12 -0500

    more updates
    Twitter works.  spotify kinda works

--- keys.js
+++ keys.js
@@ -3,4 +3,14 @@ exports.twitter = {
     consumer_secret: 'Qw8UMsUpgcR6lGbpndNdEVIOWxVWGPEIwQnmZZoipkW9O9cDEz',
     access_token_key: '904446136601235456-t45CXNJa4q3SchMy40mlrk9MbtLIT7J',
     access_token_secret: 'vZHAlzbZ41YfELG6sivePG0X4Xb3IonAspBwHkYMGiTQZ'
-   }
+}
+
+// exports.Spotify = {
+//     id: "29fb2fd2029143759230ba09d92d6e7a",
+//     secret: "4dc803c1e1474401a959c4555a4328ed"
+// }
+
+var spotify = new Spotify({
+    id: "29fb2fd2029143759230ba09d92d6e7a",
+    secret: "4dc803c1e1474401a959c4555a4328ed"
+});
--- liri.js
+++ liri.js
@@ -6,6 +6,12 @@ var keys = require("./keys.js");
 
 var client = new Twitter(keys.twitter);
 
+// var Spotify = require('spotify');
+
+var request = require('request');
+
+var fs = require('fs');
+
 var action = process.argv[2];
 console.log(action);
 
@@ -19,16 +25,22 @@ switch(action) {
     fireSpotify();
     break;
 
-    default: console.log("please enter spotify, my-tweets etc")
+    default: console.log("Please enter a command: my-tweets, spotify-this-song, movie-this, do-what-it-says")
 }
 
 function fireTwitter() {
 
     var params = {screen_name: 'XYN HA'};
-    client.get('statuses/user_timeline', params, function(error, tweet, response) {
-    if (!error) {
-    console.log(tweet);
-    }
+    client.get('statuses/user_timeline', params, function(error, tweets, response) {
+        // if (!error) {
+        //     console.log(tweets);
+        // }
+        for (var i = 0; i < tweets.length; i++) {
+            console.log(tweets[i].text);
+            if (i == 9){
+                break;
+            }
+        }
     });
 
 }
@@ -42,12 +54,42 @@ function fireSpotify() {
         secret: "4dc803c1e1474401a959c4555a4328ed"
     });
     
-    spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
-        if (err) {
-        return console.log('Error occurred: ' + err);
-    }
     
-    console.log(data); 
+    spotify.search({type: 'track', query: "December"}, function(err, data) {
+        if (!err) {
+            // return console.log('Error occurred: ' + err);
+            for (var i = 0; i < data.tracks.items.length; i++) {
+                var songData = data.tracks.items[i];
+                console.log("Artist: " + songData.artists[0].name);
+                console.log("Song: " + songData.name);
+                console.log("Preview URL: " + songData.preview_url);
+                console.log("Album: " + songData.album.name);
+                console.log("___________________________");
+                
+                //adds text to log.txt
+                fs.appendFile('log.txt', songData.artists[0].name);
+                fs.appendFile('log.txt', songData.name);
+                fs.appendFile('log.txt', songData.preview_url);
+                fs.appendFile('log.txt', songData.album.name);
+                
+
+            }
+    
+        // console.log(data); 
+        } else {
+            console.log('Oopsie');
+        }
     });
 
 }
+
+
+    //request API: 40e9cece
+    // * Title of the movie.
+    // * Year the movie came out.
+    // * IMDB Rating of the movie.
+    // * Rotten Tomatoes Rating of the movie.
+    // * Country where the movie was produced.
+    // * Language of the movie.
+    // * Plot of the movie.
+    // * Actors in the movie.
